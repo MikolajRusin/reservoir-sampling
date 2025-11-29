51003922dc1f852b6bd3d2fd71c7e1733fc9fd79
MikolajRusin <rusinmiko@gmail.com>
2025-11-29 17:35:40 +0100

    Add minimal documentation

--- streaming_data_generator.py
+++ streaming_data_generator.py
@@ -1,3 +1,7 @@
 def streaming_data_generator(n_stream: int) -> int:
+    """
+    Generates a sequence of integers from 0 up to (n_stream - 1), 
+    simulating a data stream.
+    """
     for i in range(n_stream):
         yield i
