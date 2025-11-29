ff061c857fb16f05eb396ecb21e4ec22dd0ea304
MikolajRusin <rusinmiko@gmail.com>
2025-11-29 17:45:37 +0100

    add comments and constant variables

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from streaming_data_generator import streaming_data_generator
 from reservoir_sampling import reservoir_sampling
 
-stream_generator = streaming_data_generator(100)
-reserovir = reservoir_sampling(stream=stream_generator, k=10)
-print(reserovir)
+# Define parameters
+N_STREAM = 100    # Total size of the simulated stream
+K_RESERVOIR = 10  # Desired size of the final sample
+
+# Initialize the stream generator
+streaming_generator = streaming_data_generator(N_STREAM)
+# Run the Reservoir Sampling algorithm
+reservoir = reservoir_sampling(stream=streaming_generator, k=K_RESERVOIR)
+print(reservoir)
